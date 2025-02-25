db9296c88a8cb93f7f4936060ec6291ac5597e73
Igor Gabriel Camara <142359230+iggr1@users.noreply.github.com>
2025-02-25 08:39:10 -0300

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,1 +1,12 @@
-alert("oi");
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-02-25
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+alert("oi")
