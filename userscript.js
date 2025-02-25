d524b079fc057ec7b065e8936dbd34b5ec1158da
Igor Gabriel Camara <142359230+iggr1@users.noreply.github.com>
2025-02-25 08:39:44 -0300

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -4,7 +4,7 @@
 // @version      2025-02-25
 // @description  try to take over the world!
 // @author       You
-// @match        http://*/*
+// @match        https://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
